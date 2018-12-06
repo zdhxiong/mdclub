ed188c1a5407f323df4d8a24d174d9f4e751147f
zdhxiong <zdhxiong@gmail.com>
2018-12-06 11:26:36 +0800

    添加图片管理接口

--- frontend/packages/sdk-js/src/Image.js
+++ frontend/packages/sdk-js/src/Image.js
@@ -1,8 +1,19 @@
 import {
   post,
+  patch,
+  del,
 } from './util/requestAlias';
 
 export default {
+  /**
+   * 获取图片列表
+   *
+   * GET /images
+   */
+  getList(data, success) {
+    get('/images', data, success);
+  },
+
   /**
    * 上传图片
    *
@@ -13,5 +24,41 @@ export default {
     data.append('file', file);
 
     post('/images', data, success);
+  },
+
+  /**
+   * 删除多张图片
+   *
+   * DELETE /images
+   */
+  deleteMultiple(hash, success) {
+    del('/images', { hash }, success);
+  },
+
+  /**
+   * 获取指定图片信息
+   *
+   * GET /images/{hash}
+   */
+  getOne(hash, success) {
+    get(`/images/${hash}`, success);
+  },
+
+  /**
+   * 更新指定图片信息
+   *
+   * PATCH /images/{hash}
+   */
+  updateOne(hash, data, success) {
+    patch(`/images/${hash}`, data, success);
+  },
+
+  /**
+   * 删除指定图片
+   *
+   * DELETE /images/{hash}
+   */
+  deleteOne(hash, success) {
+    del(`/images/${hash}`, success);
   },
 };
