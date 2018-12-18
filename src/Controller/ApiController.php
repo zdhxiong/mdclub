55b2c580e0abd99cd6cdca0f831d351cc7d1ec34
zdhxiong <zdhxiong@gmail.com>
2018-12-18 18:18:41 +0800

    更新 swagger-ui

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -33,7 +33,7 @@ class ApiController extends ControllerAbstracts
   <meta charset="UTF-8">
   <title>Swagger UI</title>
   <link rel="stylesheet" type="text/css" href="//fonts.loli.net/css?family=Open+Sans:400,700|Source+Code+Pro:300,600|Titillium+Web:400,600,700"/>
-  <link rel="stylesheet" type="text/css" href="//cdnjs.loli.net/ajax/libs/swagger-ui/3.16.0/swagger-ui.css"/>
+  <link rel="stylesheet" type="text/css" href="//cdnjs.loli.net/ajax/libs/swagger-ui/3.20.3/swagger-ui.css"/>
   <style>
     html { box-sizing: border-box; overflow: -moz-scrollbars-vertical; overflow-y: scroll; }
     *, *:before, *:after { box-sizing: inherit; }
@@ -42,16 +42,16 @@ class ApiController extends ControllerAbstracts
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="//cdnjs.loli.net/ajax/libs/swagger-ui/3.16.0/swagger-ui-bundle.js"> </script>
-<script src="//cdnjs.loli.net/ajax/libs/swagger-ui/3.16.0/swagger-ui-standalone-preset.js"> </script>
+<script src="//cdnjs.loli.net/ajax/libs/swagger-ui/3.20.3/swagger-ui-bundle.js"> </script>
+<script src="//cdnjs.loli.net/ajax/libs/swagger-ui/3.20.3/swagger-ui-standalone-preset.js"> </script>
 <script>
   window.onload = function() {
     window.ui = SwaggerUIBundle({
-      url: "./swagger.yaml",
       dom_id: "#swagger-ui",
       presets: [ SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset ],
       plugins: [ SwaggerUIBundle.plugins.DownloadUrl ],
-      layout: "StandaloneLayout"
+      layout: "StandaloneLayout",
+      url: "./swagger.yaml",
     });
   }
 </script>
