c13700c0676ad9e1d06239195383e6058d225582
zdhxiong <zdhxiong@gmail.com>
2019-01-18 18:13:49 +0800

    update

--- src/Service/Image.php
+++ src/Service/Image.php
@@ -467,7 +467,7 @@ class Image extends ServiceAbstracts
         foreach ($targetTypes as $type) {
             if (isset($targetIds[$type])) {
                 $targetIds[$type] = array_unique($targetIds[$type]);
-                $targets[$type] = $this->{$type . 'Service'}->getInRelationship($targetIds[$type]);
+                $targets[$type] = $this->container->{$type . 'Service'}->getInRelationship($targetIds[$type]);
             }
         }
 
