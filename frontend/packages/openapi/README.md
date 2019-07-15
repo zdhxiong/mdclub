b6c4f3059bb5ce030b89459bacc542c55c9b1340
zdhxiong <zdhxiong@gmail.com>
2019-07-15 18:25:26 +0800

    update

--- frontend/packages/openapi/README.md
+++ frontend/packages/openapi/README.md
@@ -12,3 +12,5 @@
 2. 下载 [swagger-codegen-cli](https://oss.sonatype.org/content/repositories/snapshots/io/swagger/codegen/v3/swagger-codegen-cli/3.0.9-SNAPSHOT/swagger-codegen-cli-3.0.9-20190628.091749-45.jar)，执行：
 
 `java -jar swagger-codegen-cli.jar generate -i dist/openapi.yaml -l php -o ./dist/php`
+
+也可以把 openapi.yaml 文件复制到 [https://swagger.io/editor](https://swagger.io/editor)，在线生成文档和 SDK。
--- frontend/packages/openapi/index.js
+++ frontend/packages/openapi/index.js
@@ -3,7 +3,8 @@ const YAML = require('js-yaml');
 const resolve = require('json-refs').resolveRefs;
 
 const srcPath = './src/index.yaml';
-const distPath = './dist/openapi.yaml';
+const yamlDistPath = './dist/openapi.yaml';
+const jsonDistPath = './dist/openapi.json';
 
 const doc = YAML.safeLoad(fs.readFileSync(srcPath, 'utf8').toString());
 const options = {
@@ -17,5 +18,6 @@ const options = {
 };
 
 resolve(doc, options).then((results) => {
-  fs.writeFileSync(distPath, YAML.safeDump(results.resolved));
+  fs.writeFileSync(yamlDistPath, YAML.safeDump(results.resolved));
+  fs.writeFileSync(jsonDistPath, JSON.stringify(results.resolved, null, 2));
 });
